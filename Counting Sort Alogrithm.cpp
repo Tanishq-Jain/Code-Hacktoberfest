c3cdd18724e4ce20570cd29a6d30b2f6e976f283
Tanishq-Jain <73748532+Tanishq-Jain@users.noreply.github.com>
2022-10-28 22:44:43 +0530

    Counting Sort Alogrithm.cpp

---
+++ Counting Sort Alogrithm.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#define Range 255
+void countSort(char arr[])
+{
+    char output[strlen(arr)];
+    int count[Range+1],i;
+    memset(count,0,sizeof(count));
+    for(i=0;arr[i];++i)
+    ++count[arr[i]];
+    for(i=1;i<=Range;++i)
+    count[i]+=count[i-1];
+    for(i=0;arr[i];++i)
+    {
+    	output[count[arr[i]]-1]=arr[i];
+    	--count[arr[i]];
+	}
+	for(i=0;arr[i];++i)
+	arr[i]=output[i];
+}
+int main()
+{
+	char arr[] ="Naitik";
+	countSort(arr);
+	printf("Sorted array is %s\n",arr);
+	return 0;
+}
+
